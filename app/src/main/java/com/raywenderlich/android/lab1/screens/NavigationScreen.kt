bb838cbbfd1fdb3a7845ee9e34f4772389988cdf
deadsuch <120393241+deadsuch@users.noreply.github.com>
2024-02-13 19:28:15 +0300

    Create Grid Screen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -42,6 +42,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_scaffold), Screen.Scaffold)
             NavigationButton(stringResource(id = R.string.scroll), Screen.Scrolling)
             NavigationButton(stringResource(id = R.string.List), Screen.List)
+            NavigationButton(stringResource(id = R.string.Grid), Screen.Grid)
         }
     }
 }
